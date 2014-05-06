798477d7d68b187c09e2106adb63ff42fad288da
Saleem Touny <poolmaster360@yahoo.com>
2014-05-06 11:59:07 -0500

    Update Person.java
    Javadoc - person - More completish outline

--- pImpls/Person.java
+++ pImpls/Person.java
@@ -1,8 +1,7 @@
 package pImpls;
 
 /**
- * The Elevator class represents a moveable container that navigates through various floors within the Building singleton. Elevators can hold a certain number of people and interpret
- * requests to visit certain floors. Each Elevator object runs in its own thread and will continue to run until manually shut down.
+ * The Person class represents the starting and ending floor a person will have when interacting with the elevator.
  */
 public class Person 
 {
@@ -10,10 +9,9 @@ public class Person
 	private int currentFloor;
 	
 	    /**
-     * Constructor which allows for properties such as the speed, capacity, and floor range of the elevator to be customized.
-     * @param paramCapacity The number of people this elevator can hold at a given time. When the elevator hits capacity, no more people may board the elevator.
-     * @param numFloors The number of floors that this elevator services. This value cannot be larger than the number of floors in the building that owns it, but it may be smaller
-     * @param idNumber The unique identifier number. This number cannot be negative, but it need not be in consecutive order compared to other elevators in the building.
+     * Constructor which allows for properties to be applied to a person in the building.
+     * @param floorToStop The stopping floor number that the person will stop at.
+     * @param floorToStart The starting floor number that the person will stop at.
      * @throws NegativeCapacityException if any of the values passed into it are negative
      */
 	public Person(int floorToStart, int floorToStop)
@@ -23,8 +21,8 @@ public class Person
 	}
 	
 	    /**
-     * Handles the movement of the elevator. 
-     * Will continue to run until <code>shutDownElevator()</code> is called.
+     * Handles the destination floor of elevator is is called upon.
+     * @param inDest The floor number that will be taken in as the floor destination
      */
 	private void setDestinationFloor(int inDest)
 	{
@@ -32,8 +30,8 @@ public class Person
 	}
 	
 	    /**
-     * Handles the movement of the elevator. 
-     * Will continue to run until <code>shutDownElevator()</code> is called.
+     * Allows for the setting of the current floor.
+     * @param inCur the current floor that will be set.
      */
 	private void setCurrentFloor(int inCur)
 	{
