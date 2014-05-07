d25d12e6010feb032a61b59d7a0c251841fdb7b1
Saleem Touny <poolmaster360@yahoo.com>
2014-05-07 16:33:59 -0500

    Update Direction.java
    Javadoc Finalize

--- pImpls/Direction.java
+++ pImpls/Direction.java
@@ -3,6 +3,7 @@ package pImpls;
 
 /**
  * This method switches the direction that the elevator is travelling.
+ * In the elevator class a method  is created to initialize the default direction to IDLE
  */
 public enum Direction {
 UP, DOWN, IDLE
