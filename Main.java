7f524c1ddefec40c1f35a6c8bba5f26511786ba4
Christian Simpson <christian.simpson1291@gmail.com>
2014-05-17 03:09:45 -0500

    Removed some old comments that existed solely to make sure that we were using Git correctly
    This commit will also determine whether I set the project up correctly
    on my machine since I accidentally nuked it

--- Main.java
+++ Main.java
@@ -1,23 +1,10 @@
 import pImpls.SimulationEnvironment;
-//test ocmment here 
-
-//branch conflict here!!!
-//blah blahconflicttt
-
-//fuck bitches get money
 public class Main 
 {
-    
-    
-    
-    
-    
 	public static void main(String[] args)
 	{
         try
         {
-        	//For the first deliverable, the SimulationEnvironment has been hardcoded to default to 16 floors with 4 elevators. These defaults are stored as 
-        	//constants within the ElevatorControlModule class
             SimulationEnvironment.getInstance().startSimulation();
         }
         catch (Exception e)
